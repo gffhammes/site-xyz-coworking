685fc1a4c67be43feb03bebbc62b55111573de9a
Guilherme Hammes <gffhammes@gmail.com>
2025-04-08 20:57:47 -0300

    feat: adjust differentials section

--- src/components/DifferentialsSection/DifferentialsSection.tsx
+++ src/components/DifferentialsSection/DifferentialsSection.tsx
@@ -17,6 +17,7 @@ import image3 from "../../../public/images/medium-shot-smiley-colleagues-job.jpg
 import image2 from "../../../public/images/business-people-office-break.jpg";
 import { scrollTo } from "@/utils/utils";
 import SouthIcon from "@mui/icons-material/South";
+import { AnimatePresence } from "motion/react";
 
 export interface IScrollSectionProps {}
 
@@ -121,7 +122,7 @@ export const ScrollSection = (props: IScrollSectionProps) => {
         <Box
           sx={{
             position: "sticky",
-            top: "7rem",
+            top: "12svh",
           }}
         >
           <Box
@@ -140,94 +141,7 @@ export const ScrollSection = (props: IScrollSectionProps) => {
                 position: "absolute",
                 transform: "translate(-25%, 0)",
               }}
-            ></Box>
-
-            <Container sx={{ position: "relative" }}>
-              <Stack
-                alignItems="flex-start"
-                justifyContent="center"
-                gap={2}
-                sx={{ height: circleSize }}
-              >
-                <Animate
-                  animate={{ opacity: 1, y: 0 }}
-                  initial={{ opacity: 0, y: 25 }}
-                  exit={{ opacity: 0, y: -25 }}
-                  transition={{
-                    duration: 0.75,
-                  }}
-                  key={`title ${activeIndex}`}
-                >
-                  <Typography
-                    fontSize={24}
-                    color="white"
-                    fontWeight={700}
-                    textTransform="uppercase"
-                    maxWidth="22ch"
-                  >
-                    {parse(differentialItems[activeIndex].title)}
-                  </Typography>
-                </Animate>
-
-                <Animate
-                  animate={{ opacity: 1, y: 0 }}
-                  initial={{ opacity: 0, y: 25 }}
-                  transition={{
-                    duration: 0.75,
-                    delay: 0.5,
-                  }}
-                  key={`desc ${activeIndex}`}
-                >
-                  <Typography color="white" fontWeight={300} maxWidth="30ch">
-                    {parse(differentialItems[activeIndex].description)}
-                  </Typography>
-                </Animate>
-
-                <Animate
-                  animate={{ opacity: 1, y: 0 }}
-                  initial={{ opacity: 0, y: 25 }}
-                  transition={{
-                    duration: 0.75,
-                    delay: 0.75,
-                  }}
-                  key={`button ${activeIndex}`}
-                >
-                  <Button variant="contained" sx={{ mt: 2 }}>
-                    CONHECER
-                  </Button>
-                </Animate>
-              </Stack>
-            </Container>
-
-            <Box
-              sx={{
-                height: "15rem",
-                width: "15rem",
-                position: "absolute",
-                bottom: "2rem",
-                right: 0,
-              }}
-            >
-              <Animate
-                animate={{ opacity: 1, y: 0 }}
-                initial={{ opacity: 0, y: 25 }}
-                transition={{
-                  duration: 0.75,
-                  delay: 1,
-                }}
-                key={`image ${activeIndex}`}
-              >
-                <Box
-                  sx={{
-                    backgroundImage: `url('${differentialItems[activeIndex].image}')`,
-                    backgroundSize: "cover",
-                    height: "15rem",
-                    width: "15rem",
-                    borderRadius: "15rem",
-                  }}
-                />
-              </Animate>
-            </Box>
+            />
           </Box>
 
           <Stack alignItems="center">
@@ -250,11 +164,102 @@ export const ScrollSection = (props: IScrollSectionProps) => {
             return (
               <Box
                 key={item.title}
-                height="100svh"
                 width="100%"
-                sx={{ scrollSnapAlign: "start" }}
+                sx={{ scrollSnapAlign: "start", height: "80svh", pt: "12svh" }}
                 className="teste"
-              />
+              >
+                <Container sx={{ position: "relative" }}>
+                  <Stack
+                    alignItems="flex-start"
+                    justifyContent="flex-start"
+                    gap={2}
+                    sx={{ height: circleSize, pt: "35%" }}
+                  >
+                    <AnimatePresence>
+                      <Animate
+                        initial={{ opacity: 0, y: -25 }}
+                        exit={{ opacity: 0, y: -25 }}
+                        whileInView={{ opacity: 1, y: 0 }}
+                        viewport={{ once: false }}
+                        transition={{ duration: 1 }}
+                      >
+                        <Typography
+                          fontSize={24}
+                          color="white"
+                          fontWeight={700}
+                          textTransform="uppercase"
+                          maxWidth="22ch"
+                        >
+                          {parse(item.title)}
+                        </Typography>
+                      </Animate>
+                    </AnimatePresence>
+
+                    <AnimatePresence>
+                      <Animate
+                        initial={{ opacity: 0 }}
+                        exit={{ opacity: 0 }}
+                        whileInView={{ opacity: 1 }}
+                        viewport={{ once: false }}
+                        transition={{ duration: 0.5, delay: 0.5 }}
+                      >
+                        <Typography
+                          color="white"
+                          fontWeight={300}
+                          maxWidth="30ch"
+                        >
+                          {parse(item.description)}
+                        </Typography>
+                      </Animate>
+                    </AnimatePresence>
+
+                    <AnimatePresence>
+                      <Animate
+                        initial={{ opacity: 0 }}
+                        exit={{ opacity: 0 }}
+                        whileInView={{ opacity: 1 }}
+                        viewport={{ once: false }}
+                        transition={{ duration: 0.5, delay: 0.75 }}
+                      >
+                        <Button variant="contained" sx={{ mt: 2 }}>
+                          CONHECER
+                        </Button>
+                      </Animate>
+                    </AnimatePresence>
+                  </Stack>
+
+                  <Box
+                    sx={{
+                      height: "15rem",
+                      width: "15rem",
+                      position: "absolute",
+                      bottom: 0,
+                      right: 0,
+                      transform: "translateY(20%)",
+                    }}
+                  >
+                    <AnimatePresence>
+                      <Animate
+                        initial={{ opacity: 0 }}
+                        exit={{ opacity: 0 }}
+                        whileInView={{ opacity: 1 }}
+                        viewport={{ once: false }}
+                        transition={{ duration: 0.5, delay: 0.75 }}
+                      >
+                        <Box
+                          sx={{
+                            backgroundImage: `url('${item.image}')`,
+                            backgroundSize: "cover",
+                            height: "15rem",
+                            width: "15rem",
+                            borderRadius: "15rem",
+                          }}
+                        />
+                      </Animate>
+                    </AnimatePresence>
+                  </Box>
+                </Container>
+              </Box>
             );
           })}
         </Stack>
