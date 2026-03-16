527b468e512172bc6bfc943dd06f51816a650a37
VictorSprada <victor.sprada@studiodireto.com.br>
2026-03-16 11:30:31 -0300

    changes - march prices
    alteracao preco flutuante salas de reuniao

--- src/data/joinville/services/salas-reuniao.ts
+++ src/data/joinville/services/salas-reuniao.ts
@@ -110,7 +110,7 @@ export const salasReuniao: IServiceItem = {
     ],
     plansMessage:
       "Seja um Membro X e<br/>ganhe <strong>50% de desconto</strong>",
-    floatingCtaMessage: "A partir de 75/hora",
+    floatingCtaMessage: "A partir de 78/hora",
     plansMessageTarget: membroX.slug,
   },
 };
